6bb5fdfdae4de95f4544e4ed1bdd8750b15462c1
golikp <46810318+golikp@users.noreply.github.com>
2022-05-09 14:51:48 +0200

    Add files via upload

--- gg_plots.R
+++ gg_plots.R
@@ -14,8 +14,8 @@ obs$Year <- as.factor(obs$Year)
 obs_l <- pivot_longer(obs, -c(Day, Week, Year, Music), values_to = "Observations", names_to = "Behavior")
 
 #Normalize
-n20 = 1
-n21 = 1
+n20 = 3
+n21 = 2
 obs20 <- filter(obs_l, Year == 2020)
 obs21 <- filter(obs_l, Year == 2021)
 obs20$Observations <- obs20$Observations / n20
@@ -56,7 +56,7 @@ auto1 <- ggplot(obs_auto, aes(x = Day, y = Observations)) +
     theme_bw() +
     theme(panel.grid.minor = element_blank(), panel.grid.major.y = element_blank(), panel.grid.major.x = element_line(linetype = "dotted", color = "black"))
     
-explor1 <- ggplot(obs_explor, aes(x = Day, y = Observations)) + 
+explo1 <- ggplot(obs_explor, aes(x = Day, y = Observations)) + 
     scale_x_continuous(breaks = seq(0,34, by = 7), expand = c(0, 0)) + scale_y_continuous(n.breaks = 10, expand = expansion(mult = c(0, .1))) +
 	geom_line(aes(linetype = Behavior, color = Behavior)) +
 	geom_point(aes(shape = Behavior, color = Behavior)) +
@@ -68,7 +68,7 @@ explor1 <- ggplot(obs_explor, aes(x = Day, y = Observations)) +
     theme_bw() +
     theme(panel.grid.minor = element_blank(), panel.grid.major.y = element_blank(), panel.grid.major.x = element_line(linetype = "dotted", color = "black"))    
 
-figure1 <- ggarrange(social1, auto1, explor1, labels = c("A", "B","C"), ncol =1, nrow =3)
+figure1 <- ggarrange(social1, auto1, explo1, labels = c("A", "B","C"), ncol =1, nrow =3)
 ggsave("lines.pdf", width = 20, height = 27)  
 
 #Points
@@ -99,9 +99,74 @@ social_p3 <- ggplot(obs, aes(group = Week, x = Week, y = `Agression`, fill = Mus
 	facet_grid (.~Year) +
 	theme_bw() +
 	theme(panel.grid.minor = element_blank(), panel.grid.major.y = element_blank(), panel.grid.major.x = element_blank())
-
+	
 social2 <- social_p1 + social_p2 + social_p3
-
 figure2 <- ggarrange(social1, social2, labels = c("A", "B"), ncol =1, nrow =2)
 ggsave("social.pdf", width = 20, height = 20)  
 
+auto_p1 <- ggplot(obs, aes(group = Week, x = Week, y = `Beak cleaning`, fill = Music)) + 
+	geom_point(aes(color = Music), size = 2, shape = 21, show.legend = FALSE) +
+	stat_summary(aes(color = Music), fun = median, fun.min = median, fun.max = median, geom = "crossbar", width = 1, show.legend = FALSE) +
+	stat_boxplot(aes(color = Music), geom = "errorbar", width = 0.75, show.legend = FALSE) +
+	stat_compare_means(comparisons = show_tests) +
+	facet_grid (.~Year) +
+	theme_bw() +
+    theme(panel.grid.minor = element_blank(), panel.grid.major.y = element_blank(), panel.grid.major.x = element_blank())
+	
+auto_p2 <- ggplot(obs, aes(group = Week, x = Week, y = `Grooming`, fill = Music)) + 
+	geom_point(aes(color = Music), size = 2, shape = 21, show.legend = FALSE) +
+	stat_summary(aes(color = Music), fun = median, fun.min = median, fun.max = median, geom = "crossbar", width = 1, show.legend = FALSE) +
+	stat_boxplot(aes(color = Music), geom = "errorbar", width = 0.75, show.legend = FALSE) +
+	stat_compare_means(comparisons = show_tests) +
+	facet_grid (.~Year) +
+	theme_bw() +
+    theme(panel.grid.minor = element_blank(), panel.grid.major.y = element_blank(), panel.grid.major.x = element_blank())
+  
+auto_p3 <- ggplot(obs, aes(group = Week, x = Week, y = `Eating`, fill = Music)) + 
+	geom_point(aes(color = Music), size = 2, shape = 21) +
+	stat_summary(aes(color = Music), fun = median, fun.min = median, fun.max = median, geom = "crossbar", width = 1) +
+	stat_boxplot(aes(color = Music), geom = "errorbar", width = 0.75) +
+	stat_compare_means(comparisons = show_tests) +
+	facet_grid (.~Year) +
+	theme_bw() +
+	theme(panel.grid.minor = element_blank(), panel.grid.major.y = element_blank(), panel.grid.major.x = element_blank())
+
+auto2 <- auto_p1 + auto_p2 + auto_p3
+
+figure3 <- ggarrange(auto1, auto2, labels = c("A", "B"), ncol =1, nrow =2)
+ggsave("auto.pdf", width = 20, height = 20)  
+
+explo_p1 <- ggplot(obs, aes(group = Week, x = Week, y = `Pecking objects`, fill = Music)) + 
+	geom_point(aes(color = Music), size = 2, shape = 21, show.legend = FALSE) +
+	stat_summary(aes(color = Music), fun = median, fun.min = median, fun.max = median, geom = "crossbar", width = 1, show.legend = FALSE) +
+	stat_boxplot(aes(color = Music), geom = "errorbar", width = 0.75, show.legend = FALSE) +
+	stat_compare_means(comparisons = show_tests) +
+	facet_grid (.~Year) +
+	theme_bw() +
+    theme(panel.grid.minor = element_blank(), panel.grid.major.y = element_blank(), panel.grid.major.x = element_blank())
+
+explo_p2 <- ggplot(obs, aes(group = Week, x = Week, y = `Hopping`, fill = Music)) + 
+	geom_point(aes(color = Music), size = 2, shape = 21, show.legend = FALSE) +
+	stat_summary(aes(color = Music), fun = median, fun.min = median, fun.max = median, geom = "crossbar", width = 1, show.legend = FALSE) +
+	stat_boxplot(aes(color = Music), geom = "errorbar", width = 0.75, show.legend = FALSE) +
+	stat_compare_means(comparisons = show_tests) +
+	facet_grid (.~Year) +
+	theme_bw() +
+    theme(panel.grid.minor = element_blank(), panel.grid.major.y = element_blank(), panel.grid.major.x = element_blank())
+  
+explo_p3 <- ggplot(obs, aes(group = Week, x = Week, y = `Pecking objects`, fill = Music)) + 
+	geom_point(aes(color = Music), size = 2, shape = 21) +
+	stat_summary(aes(color = Music), fun = median, fun.min = median, fun.max = median, geom = "crossbar", width = 1) +
+	stat_boxplot(aes(color = Music), geom = "errorbar", width = 0.75) +
+	stat_compare_means(comparisons = show_tests) +
+	facet_grid (.~Year) +
+	theme_bw() +
+	theme(panel.grid.minor = element_blank(), panel.grid.major.y = element_blank(), panel.grid.major.x = element_blank())
+
+explo2 <- explo_p2 + explo_p3
+
+figure3 <- ggarrange(explo1, explo2, labels = c("A", "B"), ncol =1, nrow =2)
+ggsave("explo.pdf", width = 20, height = 20)  
+
+figure4 <- ggarrange(social_p1, social_p2, social_p3, auto_p1, auto_p2, auto_p3, explo_p1, explo_p2, labels = c("A", "B","C"), ncol =3, nrow =3)
+ggsave("boxplots.pdf", width = 20, height = 20)  
